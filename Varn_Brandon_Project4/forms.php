dda857a829543f060ec7f778d93de503eaaeaf18
crimsoncrust <brandon_varn@yahoo.com>
2015-03-22 19:29:29 -0500

    updated floats
    updated floats

--- Varn_Brandon_Project4/forms.php
+++ Varn_Brandon_Project4/forms.php
@@ -9,8 +9,8 @@ $comments = $_POST['comments'];
 $Add = $_POST['Add'];
 $to = "brandon_varn@yahoo.com";
 $subject = "New Message";
-	mail ( string $to , string $subject , string $message [, string $additional_headers [, string $additional_parameters ]] )
-mail ($to, $subject, $message, "From: " . $firstname . $lastname);
+
+mail ($to, $subject, "From: " . $firstname . $lastname);
 echo "Your Message has been sent";
 
 ?>
